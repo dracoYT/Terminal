103974762d05baf31f4bf9b183d544007b68aa53
draco <44122691+dracoYT@users.noreply.github.com>
2020-09-11 09:12:59 -0500

    Terminal ON **C**

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argc, char* argv[])
+{
+
+    if(argc < 2){
+    return 0;
+}
+
+  if(!strcmp(argv[1], "print"))
+  {
+
+  }
+}
+
+//printf(argv[1]);
